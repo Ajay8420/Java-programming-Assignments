7132d7c9e3dc64b85ceffb0481928f87bfa76f73
Ajay8420 <89799818+Ajay8420@users.noreply.github.com>
2021-09-02 19:51:38 +0530

    Day-2

---
+++ Assignment-2/Day2.java
@@ -0,0 +1,8 @@
+class Addition{
+    public static void main(String args[]) {
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        int sum = x + y;
+        System.out.println("The Sum of Two Numbers is: " + sum);
+    }
+}
